4468e3540f3147d0d2edc8f9b0e69529255fa3a5
Philip <philipkuperberg@gmail.com>
2016-12-14 16:19:49 -0500

    initial commit

---
+++ KeyCenter.js
@@ -0,0 +1,165 @@
+/**
+ * KeyCenter
+ * (c) 2016 Philip Kuperberg
+ * github.com/lostclawz/keycenter
+ * 
+ * Uses a single top-level event listener for all keyboard events
+ *
+ */
+
+/**
+ * Class representing a KeyCenter instance
+ * 
+ * @class
+ */
+export class KeyCenter{
+
+	/**
+	 * @constructor
+	 * @param  {Boolean} logging Whether logging is on
+	 */
+	constructor(logging=false){
+
+		// console logging toggle
+		this.logging = logging;
+		
+		// keys we're listening for
+		this.listeningFor = {};
+		
+		// possible key modifiers
+		this.modifierKeys = ['altKey', 'ctrlKey', 'shiftKey', 'metaKey'];
+		
+		// number of registered listeners (for listener ID)
+		this.listenerCount = 0;
+
+		this.keyDown = this.keyDown.bind(this);
+		this.addListener = this.addListener.bind(this);
+		this.removeListener = this.removeListener.bind(this);
+
+		
+		// global key listener
+		document.addEventListener("keydown", this.keyDown, false);
+	}
+
+	/**
+	 * Turns console messages on or off
+	 * 
+	 * @param  {boolean}	on 	whether console messages should be visible
+	 */
+	logging = (on=true) => {
+		this.logging = on;
+	}
+
+	log = (args) => {
+		if (this.logging === true){
+			console.log.apply(null, args);
+		}
+	}
+
+	keyDown(e){
+		this.log(e, e.key);
+		if (e.key in this.listeningFor){
+			this.listeningFor[e.key].forEach((kInfo) => {
+				let ignore = false;
+				this.modifierKeys.forEach((modType) => {
+					if (kInfo[modType] !== null
+						&& kInfo[modType] !== e[modType]){
+						ignore = true;
+					}
+				});
+				if (!ignore){
+					if (kInfo.preventDefault){
+						e.preventDefault();
+					}
+					if (kInfo.stopPropagation){
+						e.stopPropagation();
+					}
+					if (typeof kInfo.callback === 'function'){
+						kInfo.callback.call(null)
+					}
+				}
+			});
+		}
+	}
+
+	/**
+	 * Registers a keystroke listener on a specific key
+	 * 
+	 * @example
+	 * // For a react component, this is usually called on componentDidMount:
+	 *	 this.keyListeners = [];
+	 *	 this.keyListeners.push(
+	 *	 	KeyCenter.addListener("n", () => {
+	 *	 		// action
+	 *	 	}, ['ctrlKey'])
+	 *	 );
+	 *
+	 * 
+	 * @param  {string}   	key             	The key to watch for, matches to e.key
+	 * @param  {function} 	callback        	Callback to fire when key is pressed
+	 * @param  {array}   	modifiers       	An array of modifier strings, can be: 'altKey', 'ctrlKey', 'shiftKey', 'metaKey'
+	 * @param  {Boolean}  	preventDefault  	Prevent default functionality (optional)
+	 * @param  {Boolean}  	stopPropagation 	Stop keystroke from bubbling up (optional)
+	 * @return {integer}                   		the ID of the listener (to use when removing the listener)
+	 */
+	addListener(key, callback, modifiers, preventDefault=false, stopPropagation=false){
+		let id = ++this.listenerCount;
+		this.log("next id:", id);
+		var modifierDefaults = {};
+		this.modifierKeys.forEach((k) => modifierDefaults[k] = null);
+		let keyData = Object.assign({
+			id,			
+			key,
+			callback,
+			preventDefault,
+			stopPropagation,
+		}, modifierDefaults, modifiers);
+
+		if (key in this.listeningFor){
+			this.listeningFor[key].push(keyData);
+		}
+		else {
+			this.listeningFor[key] = [keyData];
+		}
+		this.log(this.listeningFor);
+		return id;
+	}
+
+	/**
+	 * Remove a keystroke listener by ID
+	 * 
+	 *
+	 * @example
+	 * // In a React component, this should be called in componentWillUnmount:
+	 * componentWillUnmount() {
+ 	 *	   this.keyListeners.forEach((id) => {
+ 	 *		   KeyCenter.removeListener(id);
+ 	 *	   });
+ 	 *	   this.keyListeners = [];
+ 	 * }
+ 	 *	
+	 * @param  {integer}   	id       	The ID of the keystroke listener (returned from addListener)
+	 * @return {boolean}            	Whether listener has been successfully removed
+	 */
+	removeListener(id){
+		let removed = false;
+		if (id.constructor !== Array){
+			id = [id];
+		}
+		for (let k in this.listeningFor){
+			this.listeningFor[k].forEach((l, idx) => {
+				if (id.indexOf(l.id) >= 0){
+					this.listeningFor[k].splice(idx, 1);
+					this.log(`removed listener id ${l.id}`, this.listeningFor);
+					removed = true;
+				}
+			});
+		}
+		return removed;
+	}
+
+}
+
+const keyCenter = new KeyCenter();
+
+export default keyCenter;
--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# keycenter
-Javascript keyboard shortcut management system
+# KeyCenter
+(c) 2016 Philip Kuperberg
+github.com/lostclawz/keycenter
+Javascript keyboard shortcut management system. Uses a single top-level listener for all keyboard events.
+
+
+## Setup
+Babel requirements:
+	*	transform-class-properties
+	*	transform-object-rest-spread
+	*	transform-es2015-destructuring
+
+
+## keyCenter.addListener
+Registers a keystroke listener on a specific key.
+For a react component, this is usually called on componentDidMount:
+
+		this.keyListeners = [];
+		this.keyListeners.push(
+			KeyCenter.addListener("n", () => {
+				// action
+			}, ['ctrlKey'])
+		);
+
+@param  {string}   	key             		The key to watch for, matches to e.key
+@param  {function} 	callback        		Callback to fire when key is pressed
+@param  {array}   	modifiers       		An array of modifier strings, can be: 'altKey', 'ctrlKey', 'shiftKey', 'metaKey'
+@param  {Boolean}  	preventDefault  		Prevent default functionality (optional)
+@param  {Boolean}  	stopPropagation 		Stop keystroke from bubbling up (optional)
+@return {integer}                   		the ID of the listener (to use when removing the listener)
+
+
+## keyCenter.removeListener
+Remove a keystroke listener by ID.
+In a React component, this should be called in componentWillUnmount:
+   
+	   this.keyListeners.forEach((id) => {
+		   KeyCenter.removeListener(id);
+	   });
+	   this.keyListeners = [];
+	
+ @param  {integer}   	id       	The ID of the keystroke listener (returned from addListener)
+ @return {boolean}            		Whether listener has been successfully removed
